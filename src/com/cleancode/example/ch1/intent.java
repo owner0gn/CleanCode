ca0557b917cade3055d9b57a105797fca8cdc93e
owner0gn <owner0gn@gmail.com>
2019-02-12 14:17:06 +0900

    init commit

---
+++ src/com/cleancode/example/ch1/intent.java
@@ -0,0 +1,25 @@
+package com.cleancode.example.ch1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class intent {
+	public List<int[]> getThem(){
+		List<int[]> list1 = new ArrayList<int[]>();
+		for( int[] x : theList ) {
+			if(x[0] == 4) {
+				list1.add(x);
+			}
+			return list1;
+		}
+	}
+	
+	public List<int[]> getFlaggedCells(){
+		List<int[]> flaggedCells = new ArrayList<int[]>();
+		for(int[] cell : gameBoard) {
+			if (cell[STATUS_VALUE] == FLAGGED)
+				flaggedCells.add(Cell);
+			return flaggedCells;
+		}
+	}
+}
